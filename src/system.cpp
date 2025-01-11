5f712071691ea33752c4efbcbd51c124787aabad
kegra <nukegarara@yahoo.co.jp>
2025-01-11 19:12:51 +0900

    added GLFW

--- src/system.cpp
+++ src/system.cpp
@@ -1,9 +1,13 @@
 #include <pelican/pelican.hpp>
+#include <GLFW/glfw3.h>
+#include <AL/alut.h>
+#include <vulkan/vulkan.hpp>
+#include <glm/glm.hpp>
 
 namespace pl {
 
 void system_init() {
-
+    glfwInit();
 }
 
 bool frame_update() {

a10825e27b0e96af5c5b30df4d1a0e045b2b9fac
kegra <nukegarara@yahoo.co.jp>
2025-01-12 01:09:55 +0900

    api

--- include/pelican/pelican.hpp
+++ include/pelican/pelican.hpp
@@ -21,20 +21,26 @@ class Model {};
 Model loadModel(std::filesystem::path path);
 
 class Renderer {
+public:
     void drawModel(const Model& model, glm::mat4x4 modelMatrix);
     void setCamera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up);
     void setProjection(float horizontalAngle);
 };
+Renderer& getDefaultRenderer();
 
 class Audio {};
 Audio loadAudio(std::filesystem::path path);
 
 class Speaker {
+public:
     void set(const Audio& audio);
     void play();
     void stop();
     void setVolume(float volume);
     void setLoop(bool isLoopEnable);
+    void setPos(glm::vec3 pos);
 };
+Speaker generateSpeaker();
+void setListenerPos(glm::vec3 pos);
 
 }

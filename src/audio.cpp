734d65d6d4ca34d23d26891b1a93a293571b225d
悠 因幡 <pyonpyoninaba@outlook.jp>
2025-01-15 00:32:57 +0900

    audio流した

--- src/audio.cpp
+++ src/audio.cpp
@@ -6,16 +6,15 @@
 
 namespace pl {
 
-    Audio::Audio()
-        : bufferID(AL_NONE) // bufferID Çèâä˙âª
-    {
-    }
-
-    void Audio::loadAudio(std::filesystem::path& path) {
+    void Audio::loadAudio(std::filesystem::path& path,ALuint& audio) {
         bufferID = alutCreateBufferFromFile(path.string().c_str());
         if (bufferID == AL_NONE) {
            throw std::runtime_error("Failed to load audio file: " + path.string());
         }
+        ALuint audio_source;
+        alGenSources(1, &audio_source);
+        alSourcei(audio_source, AL_BUFFER, bufferID);
+
     }
 
     unsigned int Audio::getBufferID() const {

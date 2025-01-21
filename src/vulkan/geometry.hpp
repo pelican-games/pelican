cc025e455dd4ca6b3e3499bf16defcfc3053033d
kegra <nukegarara@yahoo.co.jp>
2025-01-21 23:39:04 +0900

    mesh loading

---
+++ src/vulkan/geometry.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include <glm/gtc/quaternion.hpp>
+#include <vulkan/vulkan.hpp>
+
+namespace pl {
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec4 tangent;
+    
+    //可変要素
+    glm::vec2 texCoord;
+    glm::vec4 color;
+    glm::uvec4 joint;
+    glm::vec4 weight;
+
+    static vk::VertexInputBindingDescription getBindingDescription() {
+        return vk::VertexInputBindingDescription(0, sizeof(Vertex), vk::VertexInputRate::eVertex);
+    }
+
+    static std::vector<vk::VertexInputAttributeDescription> getAttributeDescriptions() {
+        return {
+            vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, position)),
+            vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, normal)),
+            vk::VertexInputAttributeDescription(2, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(Vertex, tangent)),
+            vk::VertexInputAttributeDescription(3, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, texCoord)),
+            vk::VertexInputAttributeDescription(4, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(Vertex, color)),
+            vk::VertexInputAttributeDescription(5, 0, vk::Format::eR32G32B32A32Uint, offsetof(Vertex, joint)),
+            vk::VertexInputAttributeDescription(6, 0, vk::Format::eR32G32B32A32Sfloat, offsetof(Vertex, weight))
+        };
+    }
+};
+
+struct Material {
+    glm::vec4 baseColorFactor;
+    float metallicFactor;
+    float roughnessFactor;
+
+    vk::UniqueImage baseColorTexture;
+    vk::UniqueImageView baseColorTextureView;
+    vk::UniqueSampler baseColorTextureSampler;
+
+    vk::UniqueImage metallicRoughnessTexture;
+    vk::UniqueImageView metallicRoughnessTextureView;
+    vk::UniqueSampler metallicRoughnessTextureSampler;
+
+    vk::UniqueImage normalTexture;
+    vk::UniqueImageView normalTextureView;
+    vk::UniqueSampler normalTextureSampler;
+
+    vk::UniqueImage occlusionTexture;
+    vk::UniqueImageView occlusionTextureView;
+    vk::UniqueSampler occlusionTextureSampler;
+
+    vk::UniqueImage emissiveTexture;
+    vk::UniqueImageView emissiveTextureView;
+    vk::UniqueSampler emissiveTextureSampler;
+};
+
+struct Primitive {
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+    uint32_t materialIndex;
+};
+
+struct Mesh {
+    std::vector<Primitive> primitives;
+};
+
+struct Transform {
+    glm::vec3 translation;
+    glm::quat rotation;
+    glm::vec3 scale;
+};
+
+struct Object {
+    bool Instance;
+    Mesh mesh;
+    std::vector<glm::mat4> model;
+
+    static vk::VertexInputBindingDescription getBindingDescription() {
+        return vk::VertexInputBindingDescription(1, sizeof(glm::vec4) * 4, vk::VertexInputRate::eInstance);
+    }
+
+    static std::vector<vk::VertexInputAttributeDescription> getAttributeDescriptions() {
+        return {
+            vk::VertexInputAttributeDescription(7, 1, vk::Format::eR32G32B32A32Sfloat, 0),
+            vk::VertexInputAttributeDescription(8, 1, vk::Format::eR32G32B32A32Sfloat, sizeof(glm::vec4)),
+            vk::VertexInputAttributeDescription(9, 1, vk::Format::eR32G32B32A32Sfloat, 2 * sizeof(glm::vec4)),
+            vk::VertexInputAttributeDescription(10, 1, vk::Format::eR32G32B32A32Sfloat, 3 * sizeof(glm::vec4))
+        };
+    }
+};
+
+}
--- src/vulkan/model_load.cpp
+++ src/vulkan/model_load.cpp
@@ -1,6 +1,155 @@
 #define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "geometry.hpp"
+#include <filesystem>
 #include <tiny_gltf.h>
 
+template <uint32_t expectType, uint32_t expectComponentType, class F>
+void try_load_indices(const tinygltf::Model &model, const tinygltf::Primitive &primitive, F &&f) {
+    const auto accessorIndex = primitive.indices;
+    const auto &accessor = model.accessors[accessorIndex];
+    if (!(accessor.type == expectType && accessor.componentType == expectComponentType))
+        return;
 
+    const auto bufferViewIndex = accessor.bufferView;
+    const auto &bufferView = model.bufferViews[bufferViewIndex];
+    const auto bufferIndex = bufferView.buffer;
+    const auto &buffer = model.buffers[bufferIndex];
+    const auto p = buffer.data.data() + bufferView.byteOffset + accessor.byteOffset;
+    const auto stride = accessor.ByteStride(bufferView);
+    for (uint32_t i = 0; i < accessor.count; i++) {
+        f(i, p + i * stride);
+    }
+}
+
+template <uint32_t expectType, uint32_t expectComponentType, class F>
+void try_load_attribute(const char *name, const tinygltf::Model &model, const tinygltf::Primitive &primitive, F &&f) {
+    if (primitive.attributes.find(name) == primitive.attributes.end())
+        return;
+    const auto accessorIndex = primitive.attributes.at(name);
+    const auto &accessor = model.accessors[accessorIndex];
+    if (!(accessor.type == expectType && accessor.componentType == expectComponentType))
+        return;
+
+    const auto bufferViewIndex = accessor.bufferView;
+    const auto &bufferView = model.bufferViews[bufferViewIndex];
+    const auto bufferIndex = bufferView.buffer;
+    const auto &buffer = model.buffers[bufferIndex];
+    const auto p = buffer.data.data() + bufferView.byteOffset + accessor.byteOffset;
+    const auto stride = accessor.ByteStride(bufferView);
+
+    for (uint32_t i = 0; i < accessor.count; i++) {
+        f(i, p + i * stride);
+    }
+}
+
+pl::Primitive load_primitive(const tinygltf::Model &model, const tinygltf::Primitive &primitive) {
+    pl::Primitive primitiveData;
+
+    // material
+    if (primitive.material >= 0)
+        primitiveData.materialIndex = primitive.material;
+
+    // indices
+    if (primitive.indices < 0)
+        throw std::runtime_error("model load error: no index buffer");
+    const auto indCount = model.accessors[primitive.indices].count;
+    primitiveData.indices.resize(indCount);
+    try_load_indices<TINYGLTF_TYPE_SCALAR, TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE>(
+        model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.indices[i] = *reinterpret_cast<const uint8_t *>(p);
+        });
+    try_load_indices<TINYGLTF_TYPE_SCALAR, TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT>(
+        model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.indices[i] = *reinterpret_cast<const uint16_t *>(p);
+        });
+    try_load_indices<TINYGLTF_TYPE_SCALAR, TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT>(
+        model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.indices[i] = *reinterpret_cast<const uint32_t *>(p);
+        });
+
+    // attributes
+    const auto vertCount = model.accessors[primitive.attributes.at("POSITION")].count;
+    primitiveData.vertices.resize(vertCount);
+
+    try_load_attribute<TINYGLTF_TYPE_VEC3, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "POSITION", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].position = *reinterpret_cast<const glm::vec3 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC3, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "NORMAL", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].normal = *reinterpret_cast<const glm::vec3 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC4, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "TANGENT", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].tangent = *reinterpret_cast<const glm::vec4 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC2, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "TEXCOORD_0", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].texCoord = *reinterpret_cast<const glm::vec2 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC3, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "COLOR_0", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].color = glm::vec4(*reinterpret_cast<const glm::vec3 *>(p), 1.0f);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC4, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "COLOR_0", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].color = *reinterpret_cast<const glm::vec4 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC4, TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE>(
+        "JOINTS_0", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].joint = *reinterpret_cast<const glm::u8vec4 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC4, TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT>(
+        "JOINTS_0", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].joint = *reinterpret_cast<const glm::u16vec4 *>(p);
+        });
+    try_load_attribute<TINYGLTF_TYPE_VEC4, TINYGLTF_COMPONENT_TYPE_FLOAT>(
+        "WEIGHTS_0", model, primitive,
+        [&primitiveData](uint32_t i, const uint8_t *p) {
+            primitiveData.vertices[i].weight = *reinterpret_cast<const glm::vec4 *>(p);
+        });
+}
+
+void load_node(pl::Mesh &meshData, const tinygltf::Model &model, const tinygltf::Node &node) {
+    for (const auto childNodeIndex : node.children) {
+        load_node(meshData, model, model.nodes[childNodeIndex]);
+    }
+
+    if (node.mesh >= 0) {
+        const auto &mesh = model.meshes[node.mesh];
+        for (const auto &primitive : mesh.primitives) {
+            const auto primitiveData = load_primitive(model, primitive);
+
+            meshData.primitives.push_back(primitiveData);
+        }
+    }
+}
+
+pl::Mesh load_model(std::filesystem::path file_path) {
+    pl::Mesh mesh;
+
+    tinygltf::TinyGLTF loader;
+    tinygltf::Model model;
+    std::string err, warn;
+    if (!loader.LoadBinaryFromFile(&model, &err, &warn, file_path.string())) {
+        throw std::runtime_error(std::string("gltf load error: ") + err);
+    }
+
+    const auto &defaultScene = model.scenes[model.defaultScene >= 0 ? model.defaultScene : 0];
+    for (const auto nodeIndex : defaultScene.nodes) {
+        load_node(mesh, model, model.nodes[nodeIndex]);
+    }
+}

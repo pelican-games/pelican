79588ba848f7386d560df4c495f4226a9387e8b7
kegra <nukegarara@yahoo.co.jp>
2025-01-23 17:47:55 +0900

    test model

--- example/simple_render/main.cpp
+++ example/simple_render/main.cpp
@@ -5,7 +5,7 @@ int main() {
         pl::System sys(800, 600);
 
         pl::Renderer& renderer = pl::getDefaultRenderer();
-        pl::Model test_model = pl::loadModel("example/assets/fox.glb");
+        pl::Model test_model = pl::loadModel("example/assets/test.glb");
         while(sys.frameUpdate()) {
             renderer.setCamera(glm::vec3(2.0, 0.0, 0.0), glm::vec3(-1.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
             renderer.drawModel(test_model, glm::mat4x4());
